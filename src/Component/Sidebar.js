23607d540f85db5cee80e54d53a8da843f31759f
raghavy <raghavy.m.n@gmail.com>
2022-12-24 20:08:15 +0530

    sidebar hover

--- src/Component/Sidebar.js
+++ src/Component/Sidebar.js
@@ -1,96 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/Sidebar.css";
 import { Box, Typography, Avatar, Divider } from "@mui/material";
 import SchoolIcon from "@mui/icons-material/School";
 import { List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-
-import TimelineIcon from "@mui/icons-material/Timeline";
-import QueueIcon from "@mui/icons-material/Queue";
-import HomeIcon from "@mui/icons-material/Home";
-import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
-import Groups3Icon from "@mui/icons-material/Groups3";
-import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
-import BoltIcon from "@mui/icons-material/Bolt";
 import PersonIcon from "@mui/icons-material/Person";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import { sidebarData } from "../data";
 
 function Sidebar() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
-    <div>
-      <Box className="Sidebar">
-        <SchoolIcon fontSize="large" sx={{ color: "#50c878" }} />
-        <h3>
-          Edu<span>C</span>hamp
-        </h3>
-      </Box>
-      <Box p={2} role="presentation" className="side-head">
-        <Avatar>
-          <PersonIcon sx={{ color: "#444" }} />
-        </Avatar>
-        <Typography component="h6">Admin Name</Typography>
+    <div className="side">
+      <Box>
+        <div className="Sidebar">
+          <SchoolIcon fontSize="large" sx={{ color: "#50c878" }} />
+          <h3>
+            Edu<span className="span">C</span>hamp
+          </h3>
+        </div>
+        <Box p={2} role="presentation" className="side-head">
+          <Avatar>
+            <PersonIcon sx={{ color: "#50c878" }} />
+          </Avatar>
+          <Typography component="h6" sx={{ color: "#50c878" }}>
+            Admin Name
+          </Typography>
+        </Box>
       </Box>
       <Divider />
-      <List>
-        <ListItemButton
-        // sx={[
-        //   {
-        //     borderRadius: "2rem",
-        //     "&:hover": {
-        //       color: "white",
-        //       backgroundColor: "#50c878",
-        //     },
-        //   },]}
-        >
-          <ListItemIcon>
-            <HomeIcon sx={{ color: "#444" }} />
-          </ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemIcon>
-            <BoltIcon sx={{ color: "#444" }} />
-          </ListItemIcon>
-          <ListItemText primary="Quick Access" />
-        </ListItemButton>
-
-        <ListItemButton>
-          <ListItemIcon>
-            <TimelineIcon sx={{ color: "#444" }} />
-          </ListItemIcon>
-          <ListItemText primary="Dashboard" />
-        </ListItemButton>
-
-        <ListItemButton>
-          <ListItemIcon>
-            <FormatListBulletedIcon sx={{ color: "#444" }} />
-          </ListItemIcon>
-          <ListItemText primary="Student List" />
-        </ListItemButton>
-
-        <ListItemButton>
-          <ListItemIcon>
-            <QueueIcon sx={{ color: "#444" }} />
-          </ListItemIcon>
-          <ListItemText primary="Add Student" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemIcon>
-            <Groups3Icon sx={{ color: "#444" }} />
-          </ListItemIcon>
-          <ListItemText primary="Teacher List" />
-        </ListItemButton>
-
-        <ListItemButton>
-          <ListItemIcon>
-            <PersonAddAlt1Icon sx={{ color: "#444" }} />
-          </ListItemIcon>
-          <ListItemText primary="Add Teacher" />
-        </ListItemButton>
+      <List
+        sx={{
+          "& .MuiListItemButton-root:hover": {
+            borderRadius: "0.7rem",
+            bgcolor: "#50c878",
+            "&, & .MuiListItemIcon-root": {
+              color: "white",
+            },
+          },
+          "&& .Mui-selected": {
+            borderRadius: "0.7rem",
+            backgroundColor: "#50c878",
+            "&, & .MuiListItemIcon-root": {
+              color: "white",
+            },
+          },
+        }}>
+        {sidebarData.map((item, index) => {
+          return (
+            <ListItemButton selected={selectedIndex === index} key={index} onClick={() => setSelectedIndex(index)}>
+              <ListItemIcon>
+                <item.icon />
+              </ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          );
+        })}
       </List>
       <Divider />
       <ListItemButton>
         <ListItemIcon>
-          <ExitToAppIcon sx={{ color: "#444" }} />
+          <ExitToAppIcon />
         </ListItemIcon>
         <ListItemText primary="Log-Out" />
       </ListItemButton>
---
+++ src/data.js
@@ -0,0 +1,38 @@
+import TimelineIcon from "@mui/icons-material/Timeline";
+import QueueIcon from "@mui/icons-material/Queue";
+import HomeIcon from "@mui/icons-material/Home";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import Groups3Icon from "@mui/icons-material/Groups3";
+import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+import BoltIcon from "@mui/icons-material/Bolt";
+
+export const sidebarData = [
+  {
+    icon: HomeIcon,
+    text: "Home",
+  },
+  {
+    icon: BoltIcon,
+    text: "Quick Access",
+  },
+  {
+    icon: TimelineIcon,
+    text: "Dashboard",
+  },
+  {
+    icon: FormatListBulletedIcon,
+    text: "Student List",
+  },
+  {
+    icon: QueueIcon,
+    text: "Add Student",
+  },
+  {
+    icon: Groups3Icon,
+    text: "Teacher List",
+  },
+  {
+    icon: PersonAddAlt1Icon,
+    text: "Add Teacher",
+  },
+];
